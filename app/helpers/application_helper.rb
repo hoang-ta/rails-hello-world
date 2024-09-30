0247454fb82590b95539b15fc841db17368c9e5f
Hoang Ta <huyhoang.ta97@gmail.com>
2024-09-30 13:39:18 +0700

    add helper, partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(title = "sss")
+    base_title="hello_world_app"
+    if title.to_s.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
 end
